fdf23ffd249086dedf5e4db865717b3f986e8a9c
Clément Hamada <chamada@student.le-101.fr>
2020-09-16 14:44:33 +0200

    Fix Camera

--- Sources/Main.gd
+++ Sources/Main.gd
@@ -5,7 +5,7 @@ export (PackedScene) var max_jumps_decorator = preload("res://Scenes/Game Object
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Entered '", name, "' scene!")
-	$Camera.target = $Player
+	$CameraFollow.target = $Player
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
