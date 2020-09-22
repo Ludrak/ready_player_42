28211665b4bd229cb2e99d444fb6c7f77cfa335e
Ludrak <luca06.robino@gmail.com>
2020-09-22 21:54:19 +0200

    added building 2 + tweaked weapons

--- Sources/Weapons/Weapon.gd
+++ Sources/Weapons/Weapon.gd
@@ -5,14 +5,14 @@ const Cooldown = preload("res://Sources/Cooldown.gd")
 export (int) var bullet_count = 10
 export (int) var bullet_range = 10000
 export (bool) var enabled = false setget set_enabled
-export (int) var shoot_cooldown_ms = 500
+export (int) var shoot_cooldown_ms = 250
 
 onready var shoot_cooldown = Cooldown.new(shoot_cooldown_ms / 1000.0)
 
 func _on_enabled(_new_enabled):
 	pass
 
-func _on_shoot(_target):
+func _on_shoot(_target, shooter):
 	pass
 
 func set_enabled(new_enabled):
@@ -23,9 +23,9 @@ func can_shoot():
 	var bullets_left = bullet_count == -1 or bullet_count > 0
 	return bullets_left and shoot_cooldown.is_ready()
 
-func shoot(target):
+func shoot(target, shooter):
 	if enabled and can_shoot():
-		_on_shoot(target)
+		_on_shoot(target, shooter)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
